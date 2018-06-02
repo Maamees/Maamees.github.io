49ac8f28e720c6fdac8312900e8a37f2761c7a97
Spatium2 <39838450+Spatium2@users.noreply.github.com>
2018-06-02 15:12:15 +0300

    v4
    js

--- resources/js/script.js
+++ resources/js/script.js
@@ -48,25 +48,25 @@ Scroll on buttons
     $('.js--wp-1').waypoint(function(direction) {
         $('.js--wp-1').addClass('animated fadeIn');
     }, {
-        offset: '50%'
+        offset: '40%'
     });
     
      $(".js--wp-2").waypoint(function(direction) {
         $(".js--wp-2").addClass("animated fadeInUp");
     }, {
-        offset: "50%"
+        offset: "40%"
     });
     
      $(".js--wp-3").waypoint(function(direction) {
         $(".js--wp-3").addClass("animated fadeInUp");
     }, {
-        offset: "50%"
+        offset: "40%"
     });
     
      $(".js--wp-4").waypoint(function(direction) {
         $(".js--wp-4").addClass("animated pulse");
     }, {
-        offset: "50%"
+        offset: "40%"
     }); 
     
     
